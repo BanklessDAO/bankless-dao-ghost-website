a9a9740083b9061a0f4551c30f1f0828667cf642
Zach Berwaldt <zberwaldt@tutamail.com>
2021-08-25 23:47:31 -0400

    forgot to update env keys

--- lib/ghost-api.ts
+++ lib/ghost-api.ts
@@ -1,8 +1,8 @@
 import GhostContentAPI from '@tryghost/content-api';
 
 const api = new GhostContentAPI({
-    url: process.env.GHOST_URL ?? '',
-    key: process.env.GHOST_CONTENT_KEY ?? '',
+    url: process.env.NEXT_PUBLIC_GHOST_URL ?? '',
+    key: process.env.NEXT_PUBLIC_GHOST_CONTENT_KEY ?? '',
     version: "v3"
 });
 
