06005e4692a10c8bfcf3232ec1b3e99723eb4893
Jake Cooley <jakeacooley@gmail.com>
2021-09-23 02:30:19 -0700

    Complete Desktop layout for Multisig page

--- pages/multisig.tsx
+++ pages/multisig.tsx
@@ -1,6 +1,6 @@
 // Next imports
 import Head from 'next/head';
-import { chakra, Container, Flex, Box, VStack, HStack, Heading, Text, Button, Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
+import { chakra, Container, Flex, Grid, SimpleGrid, GridItem, Box, Stack, VStack, HStack, Heading, Text, Button, Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
 // Component imports
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
@@ -16,89 +16,89 @@ export default function multiSig() {
       <Navbar />
       <chakra.main className="global-main">
         <Container maxW="container.md" h="full" mb={8}>
-          <Flex direction="column">
-            <HStack>
-              <Box>
-                <VStack align="start">
-                  <Heading>About BANK</Heading>
-                  <Text>
+          <VStack spacing="20px">
+            <Stack direction="row" align="center">
+              <VStack align="start" spacing="20px">
+                <Heading m="0" as="h1" size="4xl">About BANK</Heading>
+                <VStack pr="50px" align="start" spacing="20px">
+                  <Text m="0" fontSize="14px" lineHeight="20px">
                     BANK is the native token of the Bankless DAO, which acts as a tool to coordinate activity adn is awarded to community members for participation inthe bankless movement.
                   </Text>
-                  <Button>
+                  <Button color="white" fontSize="12px">
                     View Contract
                   </Button>
-                  <Button>
+                  <Button bg="black" color="white" fontSize="12px">
                     Claim Contract
                   </Button>
                 </VStack>
-                <VStack align="start">
-                  <Heading>Ways to Earn</Heading>
-                  <Text>
+                <Heading m="0" as="h2" size="2xl">Ways to Earn</Heading>
+                <VStack pr="50px" align="start" spacing="20px">
+                  <Text m="0" fontSize="14px" lineHeight="20px">
                     To seed distribution initially, BANK was retroactively airdropped to eligible Bankless Community members.
                   </Text>
-                  <Text>
+                  <Text m="0" fontSize="14px" lineHeight="20px">
                     Full membership in the DAO is contingent on holding 35000 BANK. There are several methods of acquiring BANK.
                   </Text>
                 </VStack>
-              </Box>
+              </VStack>
               <Box className="circular-hero">
-                <Text m="auto" w="106px" fontSize="160px" fontWeight="700" color="white" borderBottom="11px solid #FF1A1A">B</Text>
+                <Text className="bank-hero">B</Text>
               </Box>
-            </HStack>
-            <HStack>
-              <VStack>
-                <Heading>Liquidity Pools</Heading>
-                <Text>BANK is available on both Sushiswap and Uniswap</Text>
-                <Button>Uniswap Pool</Button>
-                <Button>Sushiswap Pool</Button>
-              </VStack>
-              <VStack>
-                <Heading>Contribute</Heading>
-                <Text>Have an Idea to help the world go Bankless?</Text>
-                <Text>Join the Discord. Get a Guest Pass and start contributing.</Text>
-                <Button>Join Discord</Button>
-              </VStack>
-            </HStack>
-            <VStack>
-              <Heading>Multisig Vaults</Heading>
+            </Stack>
+            <Grid templateColumns="1fr 1fr">
+              <SimpleGrid templateRows="repeat(4, 1fr)" px="1rem" borderLeft="2px dotted #2F3E4E">
+                <Heading m="0" as="h3" size="m">Liquidity Pools</Heading>
+                <Text m="auto 0" textAlign="start" fontSize="13px" lineHeight="15px">BANK is available on both Sushiswap and Uniswap</Text>
+                <Button w="min-content" color="white" fontSize="12px">Uniswap Pool</Button>
+                <Button w="min-content" color="white" fontSize="12px">Sushiswap Pool</Button>
+              </SimpleGrid>
+              <SimpleGrid templateRows="repeat(4, 1fr)" px="1rem" borderLeft="2px dotted #2F3E4E">
+                <Heading m="0" as="h3" size="m">Contribute</Heading>
+                <Text m="auto 0" textAlign="start" fontSize="13px" lineHeight="15px">Have an Idea to help the world go Bankless?</Text>
+                <Text m="auto 0" textAlign="start" fontSize="13px" lineHeight="15px">Join the Discord. Get a Guest Pass and start contributing.</Text>
+                <Button w="min-content" color="white" fontSize="12px">Join Discord</Button>
+              </SimpleGrid>
+            </Grid>
+            <VStack align="start" w="full">
+              <Heading m="0" mb="20px" as="h2" size="l">Multisig Vaults</Heading>
               <Table>
                 <Thead>
-                  <Tr>
-                    <Th color="white">#</Th>
-                    <Th color="white">Guild</Th>
-                    <Th color="white">Address</Th>
+                  <Tr borderTop="1px solid white">
+                    <Th color="white" fontSize="14px" textTransform="none" whiteSpace="nowrap" align="left" pl="5px">#</Th>
+                    <Th color="white" fontSize="14px" textTransform="none" whiteSpace="nowrap" align="left" pl="5px">Guild</Th>
+                    <Th color="white" fontSize="14px" textTransform="none" whiteSpace="nowrap" align="left" pl="5px">Address</Th>
                     <Th />
                   </Tr>
                 </Thead>
                 <Tbody>
                   <Tr>
-                    <Td>1</Td>
-                    <Td>Developer's Guild</Td>
-                    <Td>0x8a3---------------------------------------9BD8F637</Td>
-                    <Td>View</Td>
+                    <Td fontSize="14px" whiteSpace="nowrap" align="left" pl="5px">1</Td>
+                    <Td fontSize="14px" whiteSpace="nowrap" align="left" pl="5px">Developer's Guild</Td>
+                    <Td fontSize="14px" whiteSpace="nowrap" align="left" pl="5px">0x8a3---------------------------------------9BD8F637</Td>
+                    <Td fontSize="14px" whiteSpace="nowrap" align="left" pl="5px">View</Td>
                   </Tr>
                   <Tr>
-                    <Td>2</Td>
-                    <Td>Writer's Guild</Td>
-                    <Td>0x8a3---------------------------------------9BD8F637</Td>
-                    <Td>View</Td>
+                    <Td fontSize="14px" whiteSpace="nowrap" align="left" pl="5px">2</Td>
+                    <Td fontSize="14px" whiteSpace="nowrap" align="left" pl="5px">Writer's Guild</Td>
+                    <Td fontSize="14px" whiteSpace="nowrap" align="left" pl="5px">0x8a3---------------------------------------9BD8F637</Td>
+                    <Td fontSize="14px" whiteSpace="nowrap" align="left" pl="5px">View</Td>
                   </Tr>
                   <Tr>
-                    <Td>3</Td>
-                    <Td>Operation's Guild</Td>
-                    <Td>0x8a3---------------------------------------9BD8F637</Td>
-                    <Td>View</Td>
+                    <Td fontSize="14px" whiteSpace="nowrap" align="left" pl="5px">3</Td>
+                    <Td fontSize="14px" whiteSpace="nowrap" align="left" pl="5px">Operation's Guild</Td>
+                    <Td fontSize="14px" whiteSpace="nowrap" align="left" pl="5px">0x8a3---------------------------------------9BD8F637</Td>
+                    <Td fontSize="14px" whiteSpace="nowrap" align="left" pl="5px">View</Td>
                   </Tr>
                   <Tr>
-                    <Td>4</Td>
-                    <Td>Designer's Guild</Td>
-                    <Td>0x8a3---------------------------------------9BD8F637</Td>
-                    <Td>View</Td>
+                    <Td fontSize="14px" whiteSpace="nowrap" align="left" pl="5px">4</Td>
+                    <Td fontSize="14px" whiteSpace="nowrap" align="left" pl="5px">Designer's Guild</Td>
+                    <Td fontSize="14px" whiteSpace="nowrap" align="left" pl="5px">0x8a3---------------------------------------9BD8F637</Td>
+                    <Td fontSize="14px" whiteSpace="nowrap" align="left" pl="5px">View</Td>
                   </Tr>
                 </Tbody>
               </Table>
             </VStack>
-          </Flex>
+          </VStack>
         </Container>
       </chakra.main>
       <Footer />
