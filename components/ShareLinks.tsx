f68c9db969dba5ac642516d6e6cb8660568e9f37
Zach Berwaldt <zberwaldt@tutamail.com>
2021-12-10 22:10:40 -0500

    fix import typo in ShareLinks.tsx

--- components/ShareLinks.tsx
+++ components/ShareLinks.tsx
@@ -2,13 +2,12 @@
 import copy from 'copy-to-clipboard';
 import { useEffect, useState } from 'react';
 import { Flex, chakra, Icon, Box } from '@chakra-ui/react';
-import { Link } from './Link';
+import Link from './Link';
 import { FaTwitter, FaFacebook, FaCopy } from 'react-icons/fa';
 import { TwitterShareButton, FacebookShareButton } from 'react-share';
 
-
 interface ShareLinksProps {
-  postTitle?: string
+  postTitle?: string;
 }
 
 // PostPage page component
@@ -31,60 +30,53 @@ const ShareLinks: React.FC<ShareLinksProps> = ({ postTitle = '' }) => {
 
   return (
     <>
-        <chakra.div
-          className="post-share-section"
+      <chakra.div
+        className="post-share-section"
+        width="100%"
+        paddingTop="40px"
+        marginBottom="15vh">
+        <Flex
+          position="relative"
+          display="inline-flex"
           width="100%"
-          paddingTop="40px"
-          marginBottom="15vh"
-        >
-          <Flex
-            position="relative"
-            display="inline-flex"
-            width="100%"
-            justifyContent="center"
-          >
-            <Link>
-              <TwitterShareButton
-                url={link}
-                title={postTitle}
-                hashtags={["Bankless"]}
-              >
-                <Icon as={FaTwitter} boxSize="24px" />
-              </TwitterShareButton>
-            </Link>
-            <Link>
-              <FacebookShareButton
-                url={link}
-                quote={postTitle}
-              >
-                <Icon as={FaFacebook} boxSize="24px" />
-              </FacebookShareButton>
-            </Link>
-            <Link onClick={copyToClipboard}>
-              {showCopyTooltip && (
-                <Box 
-                  w="120px"
-                  background="blackAlpha.50"
-                  color="white"
-                  textAlign="center"
-                  className="copy-tooltip"
-                  position="absolute"
-                  marginTop="-72px"
-                  fontSize="md"
-                  _after={{
-                    content: '""',
-                    pos: 'absolute',
-                    top: '100%',
-                    left: '50%',
-                  }}
-                >
-                  Copied!
-                </Box>
-              )}
-              <Icon as={FaCopy} boxSize="24px" />
-            </Link>
-          </Flex>
-        </chakra.div>
+          justifyContent="center">
+          <Link>
+            <TwitterShareButton
+              url={link}
+              title={postTitle}
+              hashtags={['Bankless']}>
+              <Icon as={FaTwitter} boxSize="24px" />
+            </TwitterShareButton>
+          </Link>
+          <Link>
+            <FacebookShareButton url={link} quote={postTitle}>
+              <Icon as={FaFacebook} boxSize="24px" />
+            </FacebookShareButton>
+          </Link>
+          <Link onClick={copyToClipboard}>
+            {showCopyTooltip && (
+              <Box
+                w="120px"
+                background="blackAlpha.50"
+                color="white"
+                textAlign="center"
+                className="copy-tooltip"
+                position="absolute"
+                marginTop="-72px"
+                fontSize="md"
+                _after={{
+                  content: '""',
+                  pos: 'absolute',
+                  top: '100%',
+                  left: '50%',
+                }}>
+                Copied!
+              </Box>
+            )}
+            <Icon as={FaCopy} boxSize="24px" />
+          </Link>
+        </Flex>
+      </chakra.div>
     </>
   );
 };
